a05ecab72c44ab59c0a1cc7be115170a1edf937b
Existop <81621684+Existop@users.noreply.github.com>
2021-10-20 13:13:03 +0300

    Delete CycleWhile directory

--- CycleWhile/4 Cycle5678910.py
+++
@@ -1,8 +0,0 @@
-def join_numbers_from_range(start, end):
-    i = start
-    result = ' '
-    while i <= end:
-        result = result + str(i)
-        i = i + 1
-    return result
-print(join_numbers_from_range(5, 30))
